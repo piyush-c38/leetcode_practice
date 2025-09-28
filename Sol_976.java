5592bd2238472a1ccdf89bda86eb225a6be8d295
Piyush Chandrakar <129875159+piyush-c38@users.noreply.github.com>
2025-09-28 22:04:13 +0530

    Create Sol_976.java

---
+++ Sol_976.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+
+        for(int i = nums.length-1; i>=2; i--){
+            int a = nums[i-2], b = nums[i-1], c = nums[i];
+
+            //inequality check
+            if(a+b > c){
+                // if a+b>c, area will automatically be > 0
+                return a + b + c;
+            }
+        }
+
+        return 0;
+    }
+}
